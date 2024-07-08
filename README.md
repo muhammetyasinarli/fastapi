f8e554bef4f3ee247d6a42a130d723fc3eb60abf
Yasin.Arli <Yasin.Arli@YASINARLIHP>
2024-07-08 13:58:38 +0300

    fastapi

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# fastapi
+# FastAPI
+
+## Create Virtual Environment
+
+A Python virtual environment is a folder structure that gives you everything you need to run a lightweight yet isolated Python environment.
+
+```bash
+python -m venv venv
+venv\Scripts\activate  # On Windows
+# source venv/bin/activate  # On macOS/Linux
---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
